1dedc4b57647d1a1c9a9290382b11206b54c8fc2
andie5 <andieasa@googlemail.com>
2020-04-12 00:56:35 +0100

    refactor with nested state in room obj

--- src/House.js
+++ src/House.js
@@ -2,55 +2,58 @@ import React from "react";
 
 class House extends React.Component {
   state = {
-    kitchen: true,
-    bathroom: false,
-    livingRoom: true,
-    bedroom: false,
+    rooms: {
+      kitchen: true,
+      bathroom: false,
+      livingRoom: true,
+      bedroom: false,
+    },
   };
 
   handleKitchenLight = (action) => {
-    this.state.kitchen
-      ? this.setState({ kitchen: false })
-      : this.setState({ kitchen: true });
+    this.state.rooms.kitchen
+      ? this.setState({ rooms: { ...this.state.rooms, kitchen: false } })
+      : this.setState({ rooms: { ...this.state.rooms, kitchen: true } });
   };
 
   handleBathroomLight = (action) => {
-    this.state.bathroom
-      ? this.setState({ bathroom: false })
-      : this.setState({ bathroom: true });
+    this.state.rooms.bathroom
+      ? this.setState({ rooms: { ...this.state.rooms, bathroom: false } })
+      : this.setState({ rooms: { ...this.state.rooms, bathroom: true } });
   };
 
   handleLivingLight = (action) => {
-    this.state.livingRoom
-      ? this.setState({ livingRoom: false })
-      : this.setState({ livingRoom: true });
+    this.state.rooms.livingRoom
+      ? this.setState({ rooms: { ...this.state.rooms, livingRoom: false } })
+      : this.setState({ rooms: { ...this.state.rooms, livingRoom: true } });
   };
 
   handleBedroomLight = (action) => {
-    this.state.bedroom
-      ? this.setState({ bedroom: false })
-      : this.setState({ bedroom: true });
+    this.state.rooms.bedroom
+      ? this.setState({ rooms: { ...this.state.rooms, bedroom: false } })
+      : this.setState({ rooms: { ...this.state.rooms, bedroom: true } });
   };
 
   render() {
+    console.log("this.state.rooms.kitchen: ", this.state.rooms.kitchen);
     return (
       <div>
-        Kitchen light is: {this.state.kitchen ? "ON" : "OFF"}
+        Kitchen light is: {this.state.rooms.kitchen ? "ON" : "OFF"}
         <div>
           <button onClick={this.handleKitchenLight}>Kitchen light</button>
         </div>
         <br />
-        Bathroom light is: {this.state.bathroom ? "ON" : "OFF"}
+        Bathroom light is: {this.state.rooms.bathroom ? "ON" : "OFF"}
         <div>
           <button onClick={this.handleBathroomLight}>Bathroom light</button>
         </div>
         <br />
-        Living room light is: {this.state.livingRoom ? "ON" : "OFF"}
+        Living room light is: {this.state.rooms.livingRoom ? "ON" : "OFF"}
         <div>
           <button onClick={this.handleLivingLight}>Living Room light</button>
         </div>
         <br />
-        Bedroom room light is: {this.state.bedroom ? "ON" : "OFF"}
+        Bedroom room light is: {this.state.rooms.bedroom ? "ON" : "OFF"}
         <div>
           <button onClick={this.handleBedroomLight}>Bedroom light</button>
         </div>
